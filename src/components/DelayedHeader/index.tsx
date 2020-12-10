c8296151467945d3c9dd625b477a2bd01abc501c
Joshua Byrd <phocks@gmail.com>
2020-12-10 16:56:17 +1000

    Styles in scss land

--- src/components/DelayedHeader/index.tsx
+++ src/components/DelayedHeader/index.tsx
@@ -7,6 +7,7 @@ const DelayedHeader: React.FC<DelayedHeaderProps> = () => {
   return (
     <div className={styles.root}>
       <p><span>Climate change - we get it, it’s a depressing mess.</span></p>
+      <p><span>We’ve spent the last thirty years not making the drastic changes needed to protect our way of life.</span></p>
     </div>
   );
 }
