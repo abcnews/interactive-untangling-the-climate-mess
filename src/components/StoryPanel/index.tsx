0e1f7b373524f13d7579c037b065655efe72a0e7
Joshua Byrd <phocks@gmail.com>
2020-12-23 12:18:59 +1000

    Convert a dom element into a string and then into a react component

--- src/components/StoryPanel/index.tsx
+++ src/components/StoryPanel/index.tsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import parse from "html-react-parser";
 import styles from "./styles.scss";
 
 interface StoryPanelProps {}
 
 const StoryPanel: React.FC<StoryPanelProps> = () => {
+  const [element, setElement] = useState<any>();
+
   useEffect(() => {
     console.log("StoryPanel component mounted...");
 
-    
+    var s = new XMLSerializer();
+    var d = document.querySelector(".Main p");
+    var str = d && s.serializeToString(d);
+    console.log(str);
+    const el = parse(str!);
+    console.log(el);
+
+    setElement(el);
   }, []);
 
-  return (
-    <div className={styles.root}>
-      Find me in <strong>src/components/StoryPanel</strong>
-    </div>
-  );
+  return <div className={styles.root}>{element}</div>;
 };
 
 export default StoryPanel;
