b70ff209594aae73810ee576463cd860e6b4cd7f
Joshua Byrd <phocks@gmail.com>
2020-12-23 15:57:22 +1000

    Panel!

--- src/components/StoryPanel/index.tsx
+++ src/components/StoryPanel/index.tsx
@@ -1,26 +1,58 @@
 import React, { useEffect, useState } from "react";
 import parse from "html-react-parser";
 import styles from "./styles.scss";
+import { nextUntil } from "./nextUntil";
+import { Portal } from "react-portal";
 
-interface StoryPanelProps {}
+const serializer = new XMLSerializer();
 
-const StoryPanel: React.FC<StoryPanelProps> = () => {
-  const [element, setElement] = useState<any>();
+interface StoryPanelProps {
+  startElement?: Element | null;
+  endSelector?: string;
+}
+
+const defaultProps: StoryPanelProps = {
+  startElement: document.querySelector("#panel"),
+  endSelector: "#endpanel",
+};
+
+const StoryPanel: React.FC<StoryPanelProps> = (props) => {
+  const [reactElements, setReactElements] = useState<any>();
 
   useEffect(() => {
     console.log("StoryPanel component mounted...");
 
-    var s = new XMLSerializer();
-    var d = document.querySelector(".Main p");
-    var str = d && s.serializeToString(d);
-    console.log(str);
-    const el = parse(str!);
-    console.log(el);
+    const elements = nextUntil(
+      document.querySelector("#panel"),
+      props.endSelector
+    );
 
-    setElement(el);
+    for (const el of elements) {
+      console.log(el);
+      el.style.display = "block";
+    }
+
+    const stringElements = elements.map((el) =>
+      serializer.serializeToString(el)
+    );
+    console.log(stringElements);
+
+    setReactElements(stringElements);
+
+    for (const el of elements) {
+      el.style.display = "none";
+    }
   }, []);
 
-  return <div className={styles.root}>{element}</div>;
+  return (
+    <Portal node={props.startElement}>
+      <div className={styles.root}>
+        {reactElements && reactElements.map((string) => parse(string))}
+      </div>
+    </Portal>
+  );
 };
+
+StoryPanel.defaultProps = defaultProps;
 
 export default StoryPanel;
