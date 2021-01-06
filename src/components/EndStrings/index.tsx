c3d4b0c2691a736dee99cbe0a894db4cfac5ae59
Joshua Byrd <phocks@gmail.com>
2021-01-06 11:48:47 +1000

    Font size

--- src/components/EndStrings/index.tsx
+++ src/components/EndStrings/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import SVG from "react-inlinesvg";
 
-import styles from './styles.scss';
+import styles from "./styles.scss";
 
 // Load up end tangles
 import endString1 from "./assets/EndString1.svg";
@@ -9,7 +9,6 @@ import endString2 from "./assets/EndString2.svg";
 import endString3 from "./assets/EndString3.svg";
 import endString4 from "./assets/EndString4.svg";
 import endString5 from "./assets/EndString5.svg";
-
 
 // Put them in an array
 const endStrings = [endString1, endString2, endString3, endString4, endString5];
@@ -20,22 +19,22 @@ const EndStrings: React.FC<EndStringsProps> = () => {
   return (
     <div className={styles.root}>
       {endStrings.map((svg, index) => {
-          return (
-            <div className={styles.svgLayer} key={index}>
-              <SVG
-                src={svg}
-                preProcessor={(code) => {
-                  return code;
-                }}
-                onLoad={() => {
-                  // animate();
-                }}
-              />
-            </div>
-          );
-        })}
+        return (
+          <div className={styles.svgLayer} key={index}>
+            <SVG
+              src={svg}
+              preProcessor={(code) => {
+                return code;
+              }}
+              onLoad={() => {
+                // animate();
+              }}
+            />
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default EndStrings;
