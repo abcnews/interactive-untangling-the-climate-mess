649cc692a49837b01489c5567fc1b13014e93538
Joshua Byrd <phocks@gmail.com>
2021-01-11 16:26:33 +1000

    Intersecting paragraph text

--- src/components/ParagraphObserver/index.tsx
+++ src/components/ParagraphObserver/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./styles.scss";
+import { getNextSibling } from "./helpers";
 
 interface ParagraphObserverProps {}
 
@@ -10,7 +11,9 @@ const ParagraphObserver: React.FC<ParagraphObserverProps> = () => {
   // Init some component vars
   let observer = component.observer;
 
-  const processObservation = () => {};
+  const processObservation = (entries) => {
+    console.log(entries);
+  };
 
   useEffect(() => {
     observer = new IntersectionObserver(processObservation, {
@@ -18,16 +21,33 @@ const ParagraphObserver: React.FC<ParagraphObserverProps> = () => {
       // rootMargin: `${ROOT_PULL}px 0% -${100 - props.waypoint!}%`,
     });
 
-    const paragraphStartMarkers = document.querySelectorAll(
+    const paragraphStartMarkers: any = document.querySelectorAll(
       '*[id^="paragraphtext"]'
     );
 
     console.log(paragraphStartMarkers);
 
-    // markerEls.forEach((markerEl, index: number) => {
-    //   markerEl.dataset.index = index;
-    //   observer.observe(markerEl);
-    // });
+    paragraphStartMarkers.forEach((paragraphStartElement, index: number) => {
+      // paragraphStart.dataset.index = index;
+
+      observer.observe(paragraphStartElement);
+      const paragraphEndElement = getNextSibling(
+        paragraphStartElement,
+        "#endparagraphtext"
+      );
+
+
+      const top = paragraphStartElement.getBoundingClientRect().top;
+      const bottom = paragraphEndElement.getBoundingClientRect().top;
+      const height = bottom - top;
+
+      console.log(top, bottom, `Height: ${height}`);
+
+      paragraphStartElement.className = styles.paragraphStart;
+
+      // paragraphStartElement.style.position = "absolute";
+      paragraphStartElement.style.height = `${height}px`;
+    });
 
     // // Keep track of markers we've initialised
     // // so they don't fire on load
