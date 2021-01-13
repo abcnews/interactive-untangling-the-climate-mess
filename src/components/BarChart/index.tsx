52596a30aac2634b773879dec4fff708256bb684
Joshua Byrd <phocks@gmail.com>
2021-01-13 16:54:25 +1000

    Bars component update

--- src/components/BarChart/index.tsx
+++ src/components/BarChart/index.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import styles from "./styles.scss";
 
-const Bar = () => {
-  return <div className={styles.bar}></div>;
+const defaultProps = {
+  heading: "Proportion of emissions",
+  bars: [{ title: "Enery", percent: 33 }],
 };
 
-interface BarChartProps {}
-
-const BarChart: React.FC<BarChartProps> = () => {
+const Bar = (props) => {
   return (
-    <div className={styles.root}>
-      <Bar />
+    <div className={styles.barOutline}>
+    <div className={styles.bar} style={{width: `${props.percent}%`}}></div>
     </div>
   );
 };
+
+interface BarChartProps {
+  heading?: string;
+  bars?: any[]
+}
+
+const BarChart: React.FC<BarChartProps> = ({ heading }) => {
+  return (
+    <div className={styles.root}>
+      <h3 className={styles.heading}>{heading}</h3>
+      <Bar percent={20} />
+    </div>
+  );
+};
+
+BarChart.defaultProps = defaultProps;
 
 export default BarChart;
