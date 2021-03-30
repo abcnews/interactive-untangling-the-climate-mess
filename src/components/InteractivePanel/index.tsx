a70e61e37d6628e07297afa58e4554520aa5c29e
Joshua Byrd <phocks@gmail.com>
2021-03-30 16:45:02 +1000

    Do hide feature

--- src/components/InteractivePanel/index.tsx
+++ src/components/InteractivePanel/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.scss";
 
 type InteractivePanelProps = {};
 
 const InteractivePanel: React.FC<InteractivePanelProps> = () => {
+  const [hidden, setHidden] = useState(false);
+
+  // onMount
+  useEffect(() => {
+    setTimeout(() => {
+      setHidden(true);
+    }, 1000);
+  }, []);
+
   return (
-    <div className={styles.root}>
+    <div className={`${styles.root} ${hidden ? styles.hidden : ""}`}>
       <div className={styles.panelContentContainer}>
         Interactive content will go here!!!
       </div>
