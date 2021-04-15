3be6c9a0682158837ecff12db7408675fcc4e30c
Joshua Byrd <phocks@gmail.com>
2021-04-15 10:56:11 +1000

    No vars

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-console.log("main index");
 declare var Modernizr: any;
 
 // Feature detection
 import "./modernizer";
-let notIE = false;
 
 if (Modernizr.arrow) {
   console.log("Probably not IE11...");
-  notIE = true;
 
   import("./main").then(() => {
-    console.log("...loaded main")
-  })
+    console.log("...loaded main");
+  });
 } else {
-  console.log("Probably IE11 or lower...");
+  console.log("Probably IE11 or lower... don't load.");
 
   // This unfortunately doesn't stop our JS from
   // running due to Webpack pushing all the css etc up top
@@ -22,4 +19,4 @@ if (Modernizr.arrow) {
   // );
 }
 
-export {}
+export {};
