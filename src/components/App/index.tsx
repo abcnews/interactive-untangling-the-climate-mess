cffb9d12ea99d97f6035f307eb693e4e9445b1c7
Joshua Byrd <phocks@gmail.com>
2021-04-21 15:04:49 +1000

    minor change

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -413,7 +413,6 @@ const App: React.FC<AppProps> = ({ projectName }) => {
     setQuestionCompleteness(combinedCompletenessStrings);
 
     //
-    //
 
     // console.log("User input state:", userInputState);
 
@@ -432,8 +431,8 @@ const App: React.FC<AppProps> = ({ projectName }) => {
     const mainLevel1 = mainChangeLevels[main1];
     const mainLevel2 = mainChangeLevels[main2];
 
-    console.log("Main level 1", mainLevel1)
-    console.log("Main level 2", mainLevel2)
+    console.log("Main level 1", mainLevel1);
+    console.log("Main level 2", mainLevel2);
 
     if (mainLevel1 && main2) {
       if (mainLevel1 === mainLevel2) setConvincedState("orange");
