537d22866bbe774ff8dfb1316e6f49846030bf94
Joshua Byrd <phocks@gmail.com>
2021-09-27 11:41:15 +1000

    Please update your browser

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,13 @@ declare var Modernizr: any;
 // Feature detection
 import "./modernizer";
 
+// Detect browser support
 if (Modernizr.arrow) {
-  console.log("Probably not IE11...");
-
-  import("./main").then(() => {
-    console.log("...loaded main");
-  });
+  import("./main").then(() => {});
 } else {
-  console.log("Probably IE11 or lower... don't load.");
-
-  // This unfortunately doesn't stop our JS from
-  // running due to Webpack pushing all the css etc up top
-  // throw new Error(
-  //   "Something went badly wrong... you are using an unsupported browser"
-  // );
+  console.log(
+    "Browser not supported. Please update your browser. https://www.mozilla.org/en-US/firefox/new/"
+  );
 }
 
 export {};
